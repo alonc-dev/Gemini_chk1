b8d52d4643023ad4543b3629272a35431c0bb01d
alonc-dev <alonc.dev@gmail.com>
2025-03-09 15:34:58 +0200

    not working

--- GeminiAPI.py
+++ GeminiAPI.py
@@ -1,11 +1,16 @@
 import google.generativeai as genai
+import os
 import apikey
 
-GEMINI_API_KEY = 'AIzaSyDTI3nSbxWAY8aEPARsnNYhVzZhcUZgZ8c'
-genai.configure(api_key=apikey.GEMINI_API_KEY)
-model = genai.get_model("gemini-1.5-flash")
-response = model.generate_content('Write in Java a "Hello World" console application.')
-print(response.text)
+###############################################
+def generate_text(model_name, prompt):
+    """Generates text using the Gemini API."""
+    try:
+        model = genai.GenerativeModel(model_name)
+        response = model.generate_content(prompt)
+        return response.text
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 ###############################################
 def ask_gemini(request: str) -> str:
@@ -47,6 +52,7 @@ def calculate_average(numbers):
 ###############################################
 
 def main():
+    print("----- generate_content ------")
     print("-- Start --")
     print_hi('PyCharm')
     genai_setup()
@@ -55,4 +61,12 @@ def main():
 
 ###############################################
 if __name__ == "__main__":
+    genai.configure(api_key=apikey.GEMINI_API_KEY)
+    print("----- get model ------")
+    model_name = "gemini-1.5-flash"  # Or "gemini-pro-vision" for multimodal.
+    model = genai.GenerativeModel(model_name)
+    response = model.generate_content('Write in Java a "Hello World" console application.')
+    print(response.text)
+
+    print("----- main ------")
     main()
