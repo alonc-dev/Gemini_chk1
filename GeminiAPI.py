133c1e0e608d9b6c7525e90d6e0686ef6cff087f
alonc-dev <alonc.dev@gmail.com>
2025-03-09 11:42:07 +0200

    add print

--- GeminiAPI.py
+++ GeminiAPI.py
@@ -1,15 +1,38 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+###############################################
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+  # Use a breakpoint in the code line below to debug your script.
+  print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+###############################################
+def calculate_average(numbers):
+  """
+  Calculates the average of a list of numbers.
+
+  Args:
+    numbers: A list of numbers.
+
+  Returns:
+    The average of the numbers in the list, or None if the list is empty.
+  """
+  if not numbers:
+    return None
+
+  total = sum(numbers)
+  average = total / len(numbers)
+  print(f' Average={average}')
+  return average
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+###############################################
+
+def main():
+    print("-- Start --")
     print_hi('PyCharm')
+    print("-- End --")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+###############################################
+if __name__ == "__main__":
+    main()
+
