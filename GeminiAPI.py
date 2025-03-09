631ecf500542ba9129f059eb856e78817e5201ff
alonc-dev <alonc.dev@gmail.com>
2025-03-09 13:22:20 +0200

    add apikey

--- GeminiAPI.py
+++ GeminiAPI.py
@@ -1,3 +1,27 @@
+import google.generativeai as genai
+import apikey
+
+GEMINI_API_KEY = 'AIzaSyDTI3nSbxWAY8aEPARsnNYhVzZhcUZgZ8c'
+genai.configure(api_key=apikey.GEMINI_API_KEY)
+model = genai.get_model("gemini-1.5-flash")
+response = model.generate_content('Write in Java a "Hello World" console application.')
+print(response.text)
+
+###############################################
+def ask_gemini(request: str) -> str:
+    gemini_cache = None
+    if request not in gemini_cache:
+        response = model.generate_content(request)
+        # todo: handle errors
+        gemini_cache[request]=response.text
+        # todo: save cache to file
+    return gemini_cache[request]
+
+
+###############################################
+def genai_setup():
+    print("setup")
+
 ###############################################
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -25,6 +49,7 @@ def calculate_average(numbers):
 def main():
     print("-- Start --")
     print_hi('PyCharm')
+    genai_setup()
     print("-- End --")
 
 
